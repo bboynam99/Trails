d66cfc5084d4056993327a5eae8f02c2e6753107
jnblanchet <jean.nicola.blanchet@gmail.com>
2016-09-03 17:33:15 -0400

    not sure what this does

--- client/js/app.js
+++ client/js/app.js
@@ -67,7 +67,7 @@ window.requestAnimFrame = (function(){
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame    ||
             function( callback ){
-                window.setTimeout(callback, 1000 / 20);
+                window.setTimeout(callback, 1000 / 10);
             };
 })();
 
